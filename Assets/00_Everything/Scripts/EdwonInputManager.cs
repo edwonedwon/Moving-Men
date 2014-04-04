50824f4b197ea9729b3d750396a6bca588637301
Steven Edwon <edwon@me.com>
2014-04-04 17:44:00 +0900

    player attached to plank working

---
+++ Assets/00_Everything/Scripts/EdwonInputManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class EdwonInputManager : MonoBehaviour {
+
+	void Start () {
+		InputManager.Setup();
+		InputManager.AttachDevice( new UnityInputDevice (new EdwonInControlProfile()));
+		Debug.Log( "InControl (version " + InputManager.Version + ")" );
+
+	}
+	
+	void Update () {
+		InputManager.Update();
+		var inputDevice = InputManager.ActiveDevice;
+
+		if (inputDevice.Action1)
+		{
+			Debug.Log ("pressed something");
+		}
+	}
+}
---
+++ Assets/00_Everything/Scripts/PlayerManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	public int playerIndex;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}

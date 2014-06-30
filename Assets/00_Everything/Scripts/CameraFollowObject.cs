66dbfe3e241d70a3a614a6d2a412389e73e55580
Steven Edwon <edwon@me.com>
2014-06-30 14:46:26 +0900

    camera now follows both players

---
+++ Assets/00_Everything/Scripts/CameraFollowObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowObject : MonoBehaviour {
+
+	public GameObject follow1;
+	public GameObject follow2;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		gameObject.transform.position =  (follow1.transform.position + follow2.transform.position)/2;
+	}
+}

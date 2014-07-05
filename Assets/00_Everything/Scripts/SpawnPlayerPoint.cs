a00e68545911d82e894a5126e28386fdf12c3097
Steven Edwon <edwon@me.com>
2014-07-05 17:46:07 +0900

    improvements to tutorial level

---
+++ Assets/00_Everything/Scripts/SpawnPlayerPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlayerPoint : MonoBehaviour {
+
+	public int spawnOrder;
+
+	private float gizmoSize = 1;
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = new Color(1, 0, 0, 0.5F);
+		Gizmos.DrawCube(transform.position, new Vector3(gizmoSize, gizmoSize, gizmoSize));
+	}
+}

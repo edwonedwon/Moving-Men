7d0b0b3f1a3e2038c7ed52dfb5469e63a934e669
Steven Edwon <edwon@me.com>
2014-07-08 15:58:58 +0900

    working on transparent roofs and stage 1

---
+++ Assets/00_Everything/Scripts/ClearSight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearSight : MonoBehaviour
+{
+	public GameObject mustSee;
+	public float castRadius;
+
+	void Update()
+	{
+		RaycastHit[] hits;
+		// you can also use CapsuleCastAll()
+		// TODO: setup your layermask it improve performance and filter your hits.
+		Vector3 heading = mustSee.transform.position - transform.position;
+		float distance = heading.magnitude;
+		hits = Physics.SphereCastAll(transform.position, castRadius, heading, distance);
+		DebugExtension.DebugCapsule(transform.position, mustSee.transform.position, Color.red, castRadius);
+		foreach(RaycastHit hit in hits)
+		{
+			Renderer R = hit.collider.renderer;
+			if (R == null)
+				continue; // no renderer attached? go to next hit
+			// TODO: maybe implement here a check for GOs that should not be affected like the player
+
+			if (hit.collider.tag != "Ground" && hit.collider.tag != "Floor")
+			{
+				AutoTransparent AT = R.GetComponent<AutoTransparent>();
+				if (AT == null) // if no script is attached, attach one
+				{
+					AT = R.gameObject.AddComponent<AutoTransparent>();
+				}
+				AT.BeTransparent(); // get called every frame to reset the falloff
+			}
+		}
+	}
+}
